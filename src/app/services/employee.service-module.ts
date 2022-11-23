2dd590ee6bd4822fce0376acbef368349623f4b2
werq183 <97183851+werq183@users.noreply.github.com>
2022-11-23 19:37:07 +0100

    feat: add product form

---
+++ src/app/services/employee.service-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { EmployeeService } from './employee.service';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [EmployeeService],
+  exports: []
+})
+export class EmployeeServiceModule {
+}
